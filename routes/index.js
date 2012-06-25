6b8e9bbfad72154381fa37dbd556990279e099c0
Friso Kluit <friso.kluit@gmail.com>
2012-06-25 21:19:10 +0800

    Views and routes updated

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.index = function(req, res) {
+	res.render('index', {
+		title : 'Express',
+		scripts: []
+	})
+};
+
+exports.test = function(req, res) {
+	res.render('test', {
+		data : 'test',
+		title : "Friso is here",
+		scripts : [
+				'//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+				'/socket.io/socket.io.js',
+				'/javascripts/init.js']
+	})
 };
