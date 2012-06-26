bec4b0d22651132ee4a0e76196476d958f216812
Friso Kluit <friso.kluit@gmail.com>
2012-06-26 13:40:37 +0800

    Changed client to port 80

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ io.sockets.on('connection', function(socket) {
 app.get('/', routes.index);
 app.get('/test', routes.test);
 
-app.listen(3000, function() {
+app.listen(80, function() {
 	console.log("Express server listening on port %d in %s mode",
 			app.address().port, app.settings.env);
 });
