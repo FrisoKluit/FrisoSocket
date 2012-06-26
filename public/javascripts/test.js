7674c58f234ed4fd4ae3a6298bca6c323575eee4
Friso Kluit <friso.kluit@gmail.com>
2012-06-26 22:48:56 +0800

    Added Map to /test

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,3 +1,4 @@
+
 var socket = io.connect();
 socket.on('news', function(data) {
 	console.log(data);
@@ -10,6 +11,14 @@ socket.on('time', function(data) {
 	console.log(data);
 });
 
-$(document).ready(function() {
-	alert('test');
-});
+var map;
+function initialize() {
+	var myOptions = {
+		zoom : 8,
+		center : new google.maps.LatLng(-34.397, 150.644),
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,10 @@ exports.test = function(req, res) {
 	res.render('test', {
 		data : 'test',
 		title : "Friso is here",
+		css: ['/stylesheets/test.css'],
 		scripts : [
 				'//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+				'//maps.googleapis.com/maps/api/js?sensor=false',
 				'/socket.io/socket.io.js',
 				'/javascripts/test.js']
 	})
