d64dfd66acd73d2d5f71d281355a2149a561ff12
Friso Kluit <friso.kluit@gmail.com>
2012-07-02 16:05:14 +0800

    Debugging Mongo insert

--- app.js
+++ app.js
@@ -114,17 +114,17 @@ app.post('/mobile/loc', function(req, res) {
 	console.log("Received from HTTP POST: " + req.body.data);
 	my_obj = JSON.parse(req.body.data)
 	
-	newData = {}
-	newData['lat'] = my_obj.lat
-	newData['lng'] = my_obj.lng
-	newData['acc'] = my_obj.acc
+//	newData = {}
+//	newData['lat'] = my_obj.lat
+//	newData['lng'] = my_obj.lng
+//	newData['acc'] = my_obj.acc
 	
-	wss.emit("latlng", newData)
+	wss.emit("latlng", my_obj)
 
 	// console.log("Tracking provider: " + TrackingProvider)
-	trackingProvider.save(newData, function(error, docs) {
+	trackingProvider.save(my_obj, function(error, docs) {
 		// res.redirect('/')
-		console.log("???" + newData)
+		console.log("Lat/lng inserted")
 	});
 
 	res.end("OK");
